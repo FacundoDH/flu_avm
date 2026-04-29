ff8ab05b2789bb5895d291b53a6dfed7fac5f715
Negronidas <facundodipi792002@gmail.com>
2026-04-29 20:16:47 +0200

    fgdipie: Icono de la App

--- lib/presentation/screens/numerator/numerator_screen.dart
+++ lib/presentation/screens/numerator/numerator_screen.dart
@@ -1,34 +1,5 @@
 import 'package:flutter/material.dart';
 
-/*
-class NumeratorScreen extends StatelessWidget {
-
-  int counter = 0; //cariable del contador que uniremos a 'valor'
-
-  NumeratorScreen({super.key});
-
-  @override
-
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Numerator Screen'),
-      ),
-      body: Center(
-        child: Text('Valor: $counter', style: Theme.of(context).textTheme.titleLarge,),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          counter++;
-          print(counter);
-        }, //usamos esto para incrementar 'counter'
-        child: Icon(Icons.add),
-      ),
-    );
-  }
-}
-*/
-
 class NumeratorScreen extends StatefulWidget {
   const NumeratorScreen ({super.key});
 
@@ -38,7 +9,7 @@ class NumeratorScreen extends StatefulWidget {
 
 class NumeratorScreenState extends State<NumeratorScreen> {
 
-  int counter = 0; //cariable del contador que uniremos a 'valor'
+  int counter = 0; //variable del contador que uniremos a 'valor'
 
   @override
 
