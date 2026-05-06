1ee00735d79c9c4e1f5bd878f288f1a1d5411452
Negronidas <facundodipi792002@gmail.com>
2026-05-06 18:50:44 +0200

    fgdipie: Ventana de diálogo

--- lib/presentation/screens/bands/bands_screen.dart
+++ lib/presentation/screens/bands/bands_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flu_avm/config/config.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class BandsScreen extends StatelessWidget {
   const BandsScreen({super.key});
@@ -15,6 +17,11 @@ class BandsScreen extends StatelessWidget {
         itemBuilder:(context, i) {
           return _bandTile(bands[i]);
         },
+      ),
+      floatingActionButton: FloatingActionButton(
+        elevation: 1, //sombra del elemento, diseño
+        onPressed: () => addNewBand(context),
+        child: Icon(Icons.add),
       )
     );
   }
@@ -31,4 +38,63 @@ class BandsScreen extends StatelessWidget {
       },
     );
   }
+
+  addNewBand(BuildContext context) {
+
+    final TextEditingController textController = TextEditingController();
+
+    
+    /*showDialog(
+      context: context, 
+      builder: (context) {
+        return AlertDialog(
+          title: Text('New band name'),
+          content: TextField(controller: textController),
+          actions: [
+            MaterialButton(
+              onPressed: () => addBandToList(context, textController.text), //llamamos a la función de añadir a lista y le damos el contexto y el textController
+              textColor: Colors.blue,
+              child: Text('Add'),
+            )
+          ],
+        );
+      },
+    );*/
+
+    showCupertinoDialog(
+      context: context, 
+      builder: ( BuildContext context ) => CupertinoAlertDialog(
+        title: const Text('New band name'),
+        content:  CupertinoTextField(
+        controller: textController,
+        style: TextStyle(
+          color: Theme.of(context).brightness == Brightness.dark 
+            ? Colors.white 
+            : Colors.black
+          )
+        ),
+        actions: [
+          CupertinoDialogAction(
+            isDefaultAction: true,
+            child: const Text('Add'),
+            onPressed: () {
+              addBandToList(context, textController.text);
+              context.pop();
+            }
+          ),
+          CupertinoDialogAction(
+            isDestructiveAction: true, //va a salir en rojo
+            child: const Text('Close'),
+            onPressed: () => context.pop()
+          ),
+        ],
+      )
+    );
+  }
+
+  void addBandToList(BuildContext context, String name) {
+    print(name);
+    context.pop();
+  }
 }
+
