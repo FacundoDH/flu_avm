35ae65938f54b48abaf97afc74ccc259e54bfd81
Negronidas <facundodipi792002@gmail.com>
2026-05-26 17:32:49 +0200

    fgdipie: Nuevas rutas, placeholder

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -7,6 +7,10 @@ final appRouter = GoRouter(
   routes: [
     GoRoute(
       path: '/',
+      builder: (context, state) => const WelcomeScreen(),
+    ),
+    GoRoute (
+      path: '/home',
       builder: (context, state) => const HomeScreen(),
     ),
     GoRoute(
---
+++ lib/presentation/screens/welcome/welcome_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+
+class WelcomeScreen extends ConsumerWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext contect, WidgetRef ref) {
+
+    return Placeholder();
+  }
+}
